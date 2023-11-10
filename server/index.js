9a5f219458083c6b548add2b8d3cf19e661af369
imran_9011 <imran_9011@hotmail.com>
2023-11-10 15:26:25 +0000

    added server.listen on port 3001

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-
-import { Server } from "socket.io";
+const { Server } = require("socket.io");
 
 const io = new Server(server, {
   cors: {
@@ -15,4 +14,8 @@ io.on("connection", (socket) => {
   socket.on("draw-line", ({ prevPoint, currentPoint, color }) => {
     socket.broadcast.emit("draw-line", { prevPoint, currentPoint, color });
   });
+});
+
+server.listen(3001, () => {
+  console.log("listening on port 3001");
 });
