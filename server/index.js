6524be7cf381f12c812741b4df83b5e5507691e0
imran_9011 <imran_9011@hotmail.com>
2023-11-10 18:34:59 +0000

    added sockets(client-ready, canvas-state) so if a disconnected user connects again, they will recieve the latest changes

--- server/index.js
+++ server/index.js
@@ -11,9 +11,18 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
+  socket.on("client-ready", () => {
+    socket.broadcast.emit("get-canvas-state");
+  });
+
+  socket.on("canvas-state", (state) => {
+    socket.broadcast.emit("canvas-state-from-server", state);
+  });
+
   socket.on("draw-line", ({ prevPoint, currentPoint, color }) => {
     socket.broadcast.emit("draw-line", { prevPoint, currentPoint, color });
   });
+  socket.on("clear", () => io.emit("clear"));
 });
 
 server.listen(3001, () => {
