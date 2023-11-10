ffebbb154d6a8b67ff011c931f2ddfe32b756549
imran_9011 <imran_9011@hotmail.com>
2023-11-10 15:31:33 +0000

    Revert "added server.listen on port 3001"
    This reverts commit 9a5f219458083c6b548add2b8d3cf19e661af369.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+
+import { Server } from "socket.io";
 
 const io = new Server(server, {
   cors: {
@@ -14,8 +15,4 @@ io.on("connection", (socket) => {
   socket.on("draw-line", ({ prevPoint, currentPoint, color }) => {
     socket.broadcast.emit("draw-line", { prevPoint, currentPoint, color });
   });
-});
-
-server.listen(3001, () => {
-  console.log("listening on port 3001");
 });
