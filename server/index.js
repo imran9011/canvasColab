d55f63ec6025bd1de5b2e2979f234f179518c9ef
imran_9011 <imran_9011@hotmail.com>
2023-11-10 12:18:29 +0000

    initial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+import { Server } from "socket.io";
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("draw-line", { prevPoint, currentPoint, color });
+});
