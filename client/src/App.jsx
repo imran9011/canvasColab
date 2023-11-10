8704c4e7db0f331a857a8323eaaa14cec30a37e3
imran_9011 <imran_9011@hotmail.com>
2023-11-10 15:17:32 +0000

    installed socket.io-client and react-color. Used Chromepicker component from react-color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { ChromePicker } from "react-color";
 import "./App.css";
+import { useState } from "react";
+import { io } from "socket.io-client";
 
 function App() {
-  return <h1 className="text-3xl bg-blue-500 font-bold underline">Hello world!</h1>;
+  const [colour, setColour] = useState("#955");
+  return (
+    <div>
+      <h1 className="text-3xl bg-blue-500 font-bold underline">Hello world!</h1>
+      <ChromePicker color={colour} onChange={(e) => setColour(e.hex)} />
+    </div>
+  );
 }
 
 export default App;
